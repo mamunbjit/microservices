d969686a92edd485781de9dd421b8bd43338ae33
Abdullah Al Mamun <abdullahal.mamun@bjitacademy.com>
2023-09-22 12:39:22 +0600

    department-service and employee-service created successfully

---
+++ department-service/src/main/java/com/mamun/departmentservice/model/Employee.java
@@ -0,0 +1,4 @@
+package com.mamun.departmentservice.model;
+
+public record Employee(Long id, Long departmentId, String name, int age, String position) {
+}
---
+++ department-service/src/test/java/com/mamun/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamun.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
